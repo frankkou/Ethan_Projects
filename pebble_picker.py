022f4cf3fa85b2f574e8d4a8e4b24f5ac6658c9b
Frank Kou <40340746+frankkou@users.noreply.github.com>
2021-03-21 23:11:19 -0700

    updated xlocker and created pebble_picker

---
+++ pebble_picker.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+
+import sys
+
+# list all possible shell locations
+shells = [\
+[1, 1, 0, 0], \
+[1, 0, 1, 0], \
+[1, 0, 0, 1], \
+[0, 1, 1, 0], \
+[0, 1, 0, 1], \
+[0, 0, 1, 1]]
+
+swap_guess = []
+best_score = 0
+
+num_swap = int(input("Input number of swaps: "))
+for i in range(num_swap):
+    swap_guess.append(list(map(int, list(input("Input swapped shells and guessed shells: ")))))
+
+# print(num_swap)
+# print(swap_guess)
+
+# run through each possible shell locations
+for p in range(len(shells)):
+    score = 0
+    print("Initial pebble locations are", shells[p])
+    # run through each swap_guess try
+    for t in range(num_swap):
+        tmp = shells[p][swap_guess[t][0] - 1]
+        shells[p][swap_guess[t][0] - 1] = shells[p][swap_guess[t][1] - 1]
+        shells[p][swap_guess[t][1] - 1] = tmp
+        print(shells[p])
+        if shells[p][swap_guess[t][2] - 1] == 1 :
+            score += 1
+        if shells[p][swap_guess[t][3] - 1] == 1 :
+            score += 1
+
+    print("points scored in this combination is", score)
+    if score > best_score : 
+        best_score = score
+
+print("The best score is", best_score)
